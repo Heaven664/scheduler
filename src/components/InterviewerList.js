0c92b2a0827624481daa43c80908a87d3c6061ac
Omar Hamid <omarhamid664@gmail.com>
2023-04-19 14:59:27 -0600

    InterviewerList generates list of InterviewerListItem

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,24 @@
 import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => (
+    <InterviewerListItem
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      selected={interviewer.id === props.interviewer}
+      avatar={interviewer.avatar}
+      setInterviewer={props.setInterviewer}
+    />
+  ));
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
